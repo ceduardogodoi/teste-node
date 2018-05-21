981e2ad896ea7682d9f40fe35fe96d7d47da6dfe
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-05-21 06:25:16 -0300

    Corrigindo algumas coisas...

--- produtosWeb.js
+++ produtosWeb.js
@@ -1,10 +1,8 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-    res.end("<html><body><h1>Listando os produtos</h1></body></html>");
+    res.end("<html><body><h1>Listando os Produtos</h1></body></html>");
 });
 server.listen(3000);
 
 console.log("servidor ta rodando");
-
-// curso parou em 6:23
